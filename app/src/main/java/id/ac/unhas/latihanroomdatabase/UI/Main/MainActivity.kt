a71fdddb446f257263a96d8fb51bb27b20fb949b
Nur Annisa Yusrah Putra Djaya <nurannisayusrahpdj@gmail.com>
2021-05-26 07:26:31 +0800

    Modify activity_main.xml

--- app/src/main/java/id/ac/unhas/latihanroomdatabase/UI/Main/MainActivity.kt
+++ app/src/main/java/id/ac/unhas/latihanroomdatabase/UI/Main/MainActivity.kt
@@ -1,7 +1,8 @@
-package id.ac.unhas.latihanroomdatabase
+package id.ac.unhas.latihanroomdatabase.UI.Main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import id.ac.unhas.latihanroomdatabase.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
